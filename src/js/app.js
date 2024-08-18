658006be938471b7b58e8016082f1efb1d81cfbf
Dron245 <Dron245@yandex.ru>
2024-08-18 11:25:35 +0300

    1808-1125

--- src/js/app.js
+++ src/js/app.js
@@ -5,70 +5,71 @@ import { modalOpen } from "./js_module/modalOpen.js";
 import { noUisliderFunc } from "./js_module/noUislider.js";
 import { swiperEnable } from "./js_module/swiper.js";
 
-
-
-
-
 try {
-  const slider = document.querySelectorAll('.slider__wrapper');
-  if(slider){
-
-    document.addEventListener('DOMContentLoaded',   function(){
-      slider.forEach(elem =>{
-        
-        noUisliderFunc(elem)
-      })
-    })
-  
-  }
+	const slider = document.querySelectorAll(".slider__wrapper");
+	if (slider) {
+		document.addEventListener("DOMContentLoaded", function () {
+			
+			slider.forEach((elem) => {
+				noUisliderFunc(elem);
+			});
+		});
+	}
 } catch (error) {
-  console.log(error)
+	console.log(error);
 }
 try {
-  const slider = document.querySelector('.swiper-container')
-  if(slider){
-    document.addEventListener('DOMContentLoaded', function(){
-      swiperEnable()
-    })
-  
-  }
+	const slider = document.querySelector(".swiper-container");
+	if (slider) {
+		document.addEventListener("DOMContentLoaded", function () {
+			swiperEnable();
+		});
+	}
 } catch (error) {
-  console.log(error)
+	console.log(error);
 }
 try {
-  const input = document.querySelectorAll('.inputs__input')
-  if(input){
-    changeIconFunc()
-  }
+	const input = document.querySelectorAll(".inputs__input");
+	if (input) {
+		changeIconFunc();
+	}
 } catch (error) {
-  console.log(error)
+	console.log(error);
 }
 
 try {
-  const size= document.querySelector('input[name="size"]')
-  if(size){
-    choiseSizeFunc()
-  }
+	const size = document.querySelector('input[name="size"]');
+	if (size) {
+		choiseSizeFunc();
+	}
 } catch (error) {
-  console.log(error)
+	console.log(error);
 }
 
 try {
-  const accardeons = document.querySelectorAll(".castom__select")
-  accardeons.forEach(elem => {
-      if (elem) {
-          toggleAccordion(elem)           
-      }
-  })
+	const accardeons = document.querySelectorAll(".castom__select");
+	accardeons.forEach((elem) => {
+		if (elem) {
+			toggleAccordion(elem);
+		}
+	});
 } catch (error) {
-  console.log(error)
+	console.log(error);
 }
 
 try {
-  const share = document.querySelector('.share__open')
-  if(share){
-    modalOpen()
-  }
+	const share = document.querySelector(".share__open");
+	if (share) {
+		modalOpen();
+	}
 } catch (error) {
-  console.log(error)
+	console.log(error);
 }
+// const links = document.querySelectorAll('.main__wrapper > a')
+// links.forEach(element => {
+// 	element.addEventListener('click', (event) => {
+ 
+//   event.preventDefault()
+// });
+
+// })
--- src/js/js_module/swiper.js
+++ src/js/js_module/swiper.js
@@ -1,59 +1,59 @@
-import Swiper, { Autoplay, Navigation, Pagination, Thumbs } from "swiper";
+/** @format */
+
+import Swiper, { Autoplay, Navigation, Pagination, Thumbs } from 'swiper';
 Swiper.use([Navigation, Pagination, Thumbs, Autoplay]);
 
 export function swiperEnable() {
-    const mainCatalogCarusel = new Swiper('.additionally-slider',{
-        // direction: 'horizontal',
-    slidesPerView: 'auto',
-    spaceBetween: 30,
-        navigation: {
-        prevEl: document.querySelector(".arrows__left"),
-        nextEl: document.querySelector(".arrows__right")
-      },
-    preloadImages: false,
-        lazy: {
-            loadOnTransitionStart: true,
-            loadPrevNext: true,
-        },
-        watchSlidesProgress: true,
-        watchSlidesVisibility: true,
+	const mainCatalogCarusel = new Swiper('.additionally-slider', {
+		// direction: 'horizontal',
+		slidesPerView: '1',
+		spaceBetween: 30,
+		navigation: {
+			prevEl: document.querySelector('.arrows__left'),
+			nextEl: document.querySelector('.arrows__right'),
+		},
+		loop: true,
+		preloadImages: false,
+		lazy: {
+			loadOnTransitionStart: true,
+			loadPrevNext: true,
+		},
+		watchSlidesProgress: true,
+		watchSlidesVisibility: true,
+	});
 
-    })
-  
-    
-
-    // var swiper = new Swiper('.additionally-slider', {
-    //     slidesPerView: 2,
-    //     spaceBetween: 20,   
-    //     pagination: {
-    //       el: '.swiper-pagination',
-    //       clickable: true,
-    //     },
-    //     navigation: {
-    //       nextEl: '.swiper-button-next',
-    //       prevEl: '.swiper-button-prev',
-    //     },
-    //     on: {
-    //       init: function () {
-    //         for (let i = 0; i < this.slidesPerView; i++) {
-    //           const slide = swiper.slides[swiper.activeIndex + i];
-    //           const iframe = slide.querySelector('iframe');
-    //           loadIframe(iframe);
-    //         }
-    //       },
-    //       slideChange: function () {
-    //         const totalSlides = swiper.slides.length;
-    //         swiper.slides.forEach((slide, index) => {
-    //           const iframe = slide.querySelector('iframe');
-    //           if (index >= swiper.activeIndex && index < swiper.activeIndex + swiper.slidesPerView) {
-    //             loadIframe(iframe);
-    //           } else {
-    //             unloadIframe(iframe);
-    //           }
-    //         });
-    //       },
-    //     },
-    //   });
+	// var swiper = new Swiper('.additionally-slider', {
+	//     slidesPerView: 2,
+	//     spaceBetween: 20,
+	//     pagination: {
+	//       el: '.swiper-pagination',
+	//       clickable: true,
+	//     },
+	//     navigation: {
+	//       nextEl: '.swiper-button-next',
+	//       prevEl: '.swiper-button-prev',
+	//     },
+	//     on: {
+	//       init: function () {
+	//         for (let i = 0; i < this.slidesPerView; i++) {
+	//           const slide = swiper.slides[swiper.activeIndex + i];
+	//           const iframe = slide.querySelector('iframe');
+	//           loadIframe(iframe);
+	//         }
+	//       },
+	//       slideChange: function () {
+	//         const totalSlides = swiper.slides.length;
+	//         swiper.slides.forEach((slide, index) => {
+	//           const iframe = slide.querySelector('iframe');
+	//           if (index >= swiper.activeIndex && index < swiper.activeIndex + swiper.slidesPerView) {
+	//             loadIframe(iframe);
+	//           } else {
+	//             unloadIframe(iframe);
+	//           }
+	//         });
+	//       },
+	//     },
+	//   });
 }
 
 // function loadIframe(iframe) {
